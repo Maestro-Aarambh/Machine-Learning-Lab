451c8653c2df6b94f9928e351550f8c153a33eaa
Aarambh Vaish <aarambhvaish1708@gmail.com>
2025-03-03 04:16:07 +0530

    a part done

---
+++ q1.py
@@ -0,0 +1,11 @@
+import statistics as stats
+import math
+import numpy as np
+import scipy as sc
+x=float(input("Enter a decimal number "))
+y=int(input("Enter an integer number "))
+print("Floor value is ",math.floor(x))
+print("Ceil value is ",math.ceil(x))
+print("Square root is",math.sqrt(x))
+print("Integer Square root is",math.isqrt(y))
+print("Greatest Common Divisor is ",math.gcd(x,y))
